e523e010929f8c187544e3ef2d3f3384cb9817db
Mehdi Mediouni <memehdy.5@gmail.com>
2026-03-27 22:55:28 +0000

    config incremental model fct_orders updated

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,6 +1,9 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+         unique_key = 'order_id',
+    incremental_strategy = 'merge',
+    on_schema_change = 'sync_all_columns',
     )
 }}
 with orders as  (
